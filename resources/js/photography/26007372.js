faf5f8f0418927ce33ee619675803c5528d98bb0
tq <qitang@bjtu.edu.cn>
2021-12-23 22:33:20 +0800

    init photography

---
+++ resources/js/photography/26007372.js
@@ -0,0 +1,19 @@
+!function (c, l, a, r, i, t, y) {
+    a[c] = a[c] || function () {
+        (a[c].q = a[c].q || []).push(arguments)
+    };
+    (new Image).src = "https://c.clarity.ms/c.gif";
+    a[c]("set", "_s", "uet");
+    a[c]("set", "_u", "26007372");
+    if (a[c].v || a[c].t) return a[c]("event", c, "dup." + i.projectId);
+    a[c].t = !0, (t = l.createElement(r)).async = !0, t.src = "https://d.clarity.ms/s/0.6.31/clarity.js", (y = l.getElementsByTagName(r)[0]).parentNode.insertBefore(t, y), a[c]("start", i), a[c].q.unshift(a[c].q.pop())
+}("clarity", document, window, "script", {
+    "projectId": "9nmf53kyfv",
+    "upload": "https://d.clarity.ms/collect",
+    "expire": 365,
+    "cookies": ["_uetmsclkid", "_uetvid"],
+    "track": true,
+    "lean": false,
+    "fallback": "https://www.clarity.ms/eus/collect",
+    "content": true
+});
