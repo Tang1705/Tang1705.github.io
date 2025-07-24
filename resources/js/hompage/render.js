f60b90d7d8c1a8c8f65e546937819ecebb979b73
唐麒 <tangqi@tangqideMacBook-Pro.local>
2025-07-24 19:05:13 +0800

    update

--- resources/js/hompage/render.js
+++ resources/js/hompage/render.js
@@ -3,19 +3,19 @@
  */
 function initPage() {
     // 加载并渲染论文数据
-    fetch('https://tang5618.com/resources/data/publications.json')
+    fetch('./resources/data/publications.json')
         .then(response => response.json())
         .then(data => renderPublications(data))
         .catch(error => console.error('Error loading publications:', error));
 
     // 加载并渲染项目数据
-    fetch('https://tang5618.com/resources/data/projects.json')
+    fetch('./resources/data/projects.json')
         .then(response => response.json())
         .then(data => renderProjects(data))
         .catch(error => console.error('Error loading projects.json:', error));
 
     // 加载并渲染荣誉数据
-    fetch('https://tang5618.com/resources/data/honors.json')
+    fetch('./resources/data/honors.json')
         .then(response => response.json())
         .then(data => renderHonors(data))
         .catch(error => console.error('Error loading honors:', error));
@@ -37,36 +37,25 @@ function renderPublications(publications) {
                 : `<span> ${author.name} </span>`;
         }).join('<span class="separator">,</span>');
 
-        // 生成链接HTML
-        let linksHtml = '';
-        // if (pub.links.project && pub.links.project !== ".") {
-        //     linksHtml += `<a class="action" href="${pub.links.project}">Project Page</a>`;
-        // }
-        // if (pub.links.paper && pub.links.paper !== ".") {
-        //     linksHtml += `<a class="action" href="${pub.links.paper}">Paper</a>`;
-        // }
-        // if (pub.links.code && pub.links.code !== ".") {
-        //     linksHtml += `<a class="action" href="${pub.links.code}">Code</a>`;
-        // }
-        // if (pub.links.honor && pub.links.honor !== ".") {
-        //     linksHtml += `<a class="action" href="${pub.links.honor}">Honor</a>`;
-        // }
+        const tagsHtml=pub.tags.map(tag => {
+            return `<span class="publication-tag" style="margin-right: -0.2em">${tag}</span>`}).join('');
 
-        // 拼接论文HTML
+
         const pubHtml = `
-            <div class="publications row w-100" data-category="${pub.category}">
-                <div class="col w-100">
-                    <div class="publication row  w-100">
-                        <div class="section-1 w-100">
-                         <i class="ai ai-paperclip action"></i><span>${pub.title}</span>
-                            <br>
-                            <span class="text-muted publication-name">${pub.venue}</span>
-                        </div>
-                        <div class="section-2 text-muted w-100">${authorsHtml}</div>
-                        <div class="actions">${linksHtml}</div>
-                    </div>
-                </div>
+        <div class="publication-item show" data-category="${pub.category}" style="margin-left: -2em">
+            <div class="publication-header" style="margin-left: 2.5em">
+                <span class="badge-ccf badge-${pub.category}" style="margin-right: 0.5em">${pub.category}</span>
+                ${tagsHtml}                
             </div>
+            <h3 class="publication-title" style="margin-left: 2em">${pub.title} <i class="fas fa-file-pdf"></i></h3>
+            <div class="publication-venue" style="margin-left: 2.5em">${pub.venue}</div>
+            <div class="publication-authors" style="margin-left: 2.5em">${authorsHtml}</div>
+<!--            <div class="publication-links" style="margin-left: 2.5em">-->
+<!--                <a href="#"><i class="fas fa-file-pdf"></i> PDF</a>-->
+<!--                <a href="#"><i class="fab fa-github"></i> Code</a>-->
+<!--                <a href="#"><i class="fas fa-link"></i> Project Page</a>-->
+<!--            </div>-->
+        </div>
         `;
         container.innerHTML += pubHtml;
     });
@@ -90,54 +79,47 @@ function renderProjects(projects) {
 
         // 生成链接HTML
         let linksHtml = '';
-        if (project.links.project && project.links.project !== ".") linksHtml += `<a class="action" href="${project.links.project}">project page</a>`;
-        if (project.links.poster && project.links.poster !== ".") linksHtml += `<a class="action" href="${project.links.poster}">Poster</a>`;
-        if (project.links.code && project.links.code !== ".") linksHtml += `<a class="action" href="${project.links.code}">Code</a>`;
-        if (project.links.pcDemo && project.links.pcDemo !== ".") linksHtml += `<a class="action" href="${project.links.pcDemo}">PC Demo</a>`;
-        if (project.links.sdkDemo && project.links.sdkDemo !== ".") linksHtml += `<a class="action" href="${project.links.sdkDemo}">SDK Demo</a>`;
-
-        // 拼接项目HTML
+        if (project.links.project && project.links.project !== ".") linksHtml += `<a class="action" href="${project.links.project}"><i class="fab fa-chrome"></i> Project Page</a>`;
+        if (project.links.poster && project.links.poster !== ".") linksHtml += `<a class="action" href="${project.links.poster}"><i class="fab fa-slideshare"></i> Poster</a>`;
+        if (project.links.code && project.links.code !== ".") linksHtml += `<a class="action" href="${project.links.code}"><i class="fab fa-github"></i> Code</a>`;
+        if (project.links.pcDemo && project.links.pcDemo !== ".") linksHtml += `<a class="action" href="${project.links.pcDemo}"><i class="fab fa-windows"></i> PC Demo</a>`;
+        if (project.links.sdkDemo && project.links.sdkDemo !== ".") linksHtml += `<a class="action" href="${project.links.sdkDemo}"><i class="fab fa-github"></i> SDK Demo</a>`;
         const projectHtml = `
-            <div class="row w-100" style="margin-left:-1em ">
-            <div class="col w-100">
-                <div class="publication row  w-100">
-                    <div class="section-1 w-100">
-                        <span>${project.title}</span>
-                        <br>
-                        <span class="text-muted publication-name">${project.venue}</span>
+        <div class="project-card">
+            <h3 class="project-title">${project.title}</h3>
+            <div class="project-description">
+                ${project.venue}
+                <br>
+                <p style="font-style: italic;">${membersHtml}</p>
+            </div>
+            <div class="project-links">
+                ${linksHtml}
+            </div>`;
+
+            container.innerHTML += projectHtml;
+            });
+            }
+
+            /**
+            * 渲染荣誉列表
+            * @param {Array} honors - 荣誉数据数组
+            */
+            function renderHonors(honors) {
+            const container = document.getElementById('honors-container');
+            container.innerHTML = ''; // 清空容器
+
+            honors.forEach(honor => {
+            const honorHtml = `
+                <div class="honor-item">
+                    <div class="honor-icon">
+                        <i class="fas fa-trophy"></i>
                     </div>
-                    <div class="section-2 text-muted w-100">${membersHtml}</div>
-                    <div class="actions">${linksHtml}</div>
-                </div>
-            </div>
-            </div>
-        `;
-        container.innerHTML += projectHtml;
-    });
-}
-
-/**
- * 渲染荣誉列表
- * @param {Array} honors - 荣誉数据数组
- */
-function renderHonors(honors) {
-    const container = document.getElementById('honors-container');
-    container.innerHTML = ''; // 清空容器
-
-    honors.forEach(honor => {
-        const honorHtml = `
-<!--            <div class="row w-100" style="margin-left:-1em ">-->
-<!--            <div class="col w-100">-->
-                <div class="publication row  w-100">
-                    <div class="section-1 w-100">
-                        <span>
-                            <span style="color: #6c757d!important"> <i class="fas fa-trophy action"></i> [${honor.date}]</span> <b>${honor.title}</b>
-                        </span>
+                    <div class="honor-content">
+                        <span class="honor-title" style="color: #6c757d!important">[${honor.date}]</span> <span class="honor-description">${honor.title}</span>
                     </div>
                 </div>
-<!--            </div>-->
-<!--            </div>-->
+                
         `;
-        container.innerHTML += honorHtml;
-    });
-}
+            container.innerHTML += honorHtml;
+        });
+        }
